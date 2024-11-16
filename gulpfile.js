3cedb0944efaecf7c1cba058db6c5dd00947d177
Maria Paramonova <paramasha932224@yandex.ru>
2024-11-16 19:45:48 +0300

    code-continue 37 slozhno-sosredotochitsya

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat-css');
+const plumber = require('gulp-plumber');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+function html() {
+  return gulp.src('src/**/*.html')
+        .pipe(plumber())
+                .pipe(gulp.dest('dist/'))
+}
+
+function css() {
+  return gulp.src('src/blocks/**/*.css')
+        .pipe(plumber())
+        .pipe(concat('bundle.css'))
+                .pipe(gulp.dest('dist/'))
+}
+
+function images() {
+  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+            .pipe(gulp.dest('dist/images'))
+}
+
+function clean() {
+  return del('dist');
+}
+
+exports.html = html;
+exports.css = css;
+exports.images = images;
+exports.clean = clean;
