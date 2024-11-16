b2439a7787f5fd1b73f313c285b12990939201c7
Maria Paramonova <paramasha932224@yandex.ru>
2024-11-16 21:03:32 +0300

    code-continue 38 slozhno-sosredotochitsya

--- gulpfile.js
+++ gulpfile.js
@@ -22,11 +22,29 @@ function images() {
             .pipe(gulp.dest('dist/images'))
 }
 
-function clean() {
-  return del('dist');
+function fonts() {
+  return gulp.src('src/fonts/**/*.{woff,woff2}')
+    .pipe(gulp.dest('dist/fonts'));
 }
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist',
+    }
+  });
+
+  gulp.watch('src/**/*.html', html).on('change', browserSync.reload);
+  gulp.watch('src/blocks/**/*.css', css).on('change', browserSync.reload);
+  gulp.watch('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}', images).on('change', browserSync.reload);
+}
+
+exports.serve = serve;
+
+const build = gulp.series(clean, gulp.parallel(html, css, images, fonts));
+exports.build = build;
 
 exports.html = html;
 exports.css = css;
 exports.images = images;
-exports.clean = clean;
+exports.fonts = fonts;
